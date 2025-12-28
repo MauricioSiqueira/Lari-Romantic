8d6c2cbc96f237595776a70a036c2447844ee294
MauricioSiqueira <mauricegdev@gmail.com>
2025-12-28 02:11:08 -0300

    BackgroundImg

--- love-page/src/App.tsx
+++ love-page/src/App.tsx
@@ -2,12 +2,13 @@
 import Carousel from './components/Carousel'
 import Letter from './components/Letter'
 import SpotifyPlayer from './components/SpotifyPlayer'
+import backgroundImage from './components/assets/background.jpg'
 
 export default function App() {
   return (
     <div
       className="min-h-screen bg-cover bg-center flex flex-col items-center justify-center"
-      style={{ backgroundImage: "url('https://images.unsplash.com/photo-1506744038136-46273834b3fb')" }}
+      style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className="backdrop-blur-md bg-black/30 w-full h-full absolute inset-0" />
 
